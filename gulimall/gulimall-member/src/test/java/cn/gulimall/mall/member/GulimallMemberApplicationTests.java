e2d7c68a9a2858f86b2ceb66c567f8811dccc184
tanghaiwen <tanghaiwen@qq.com>
2020-09-03 12:33:14 +0800

    【初始化】谷粒商城第一次提交

---
+++ gulimall/gulimall-member/src/test/java/cn/gulimall/mall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gulimall.mall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
